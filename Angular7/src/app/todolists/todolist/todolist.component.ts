8c97a033b724ce43e5c35fae8fd0fd8e199af70e
frianelli-lopes <frianelli.lopes@gmail.com>
2019-05-28 15:05:33 -0300

    Acertando a inclusão

--- Angular7/src/app/todolists/todolist/todolist.component.ts
+++ Angular7/src/app/todolists/todolist/todolist.component.ts
@@ -1,67 +1,64 @@
-import { Todolist } from './../../shared/todolist.model';
-import { TodolistService } from './../../shared/todolist.service';
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
-
+import { Todolist } from "./../../shared/todolist.model";
+import { TodolistService } from "./../../shared/todolist.service";
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
-  selector: 'app-todolist',
-  templateUrl: './todolist.component.html',
+  selector: "app-todolist",
+  templateUrl: "./todolist.component.html",
   styles: []
 })
 export class TodolistComponent implements OnInit {
-
-  constructor(private service: TodolistService,
-    private toastr: ToastrService) { }
+  constructor(
+    private service: TodolistService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit() {
     this.resetForm();
   }
 
-
   resetForm(form?: NgForm) {
-    if (form != null)
-      form.form.reset();
+    if (form != null) form.form.reset();
     this.service.formData = {
       ToDoListId: 0,
-      Titulo: '',
-      Concluido: 'N'
-    }
+      Titulo: "",
+      Concluido: "S"
+    };
   }
 
   onSubmit(form: NgForm) {
-    if (this.service.formData.ToDoListId == 0)
-      this.insertRecord(form);
-    else
-      this.updateRecord(form);
+    console.log(form.value);
+    if (this.service.formData.ToDoListId == 0) this.insertRecord(form);
+    else this.updateRecord(form);
   }
 
   insertRecord(form: NgForm) {
-    this.service.postTodolist().subscribe(
+    this.service.postTodolist(form.value).subscribe(
       res => {
-        debugger;
+        //debugger;
         this.resetForm(form);
-        this.toastr.success('Inserido com sucesso', 'Registro de Tarefa');
+        this.toastr.success("Inserido com sucesso", "Registro de Tarefa");
         this.service.refreshList();
       },
       err => {
-        debugger;
+        //debugger;
         console.log(err);
       }
-    )
+    );
   }
+
   updateRecord(form: NgForm) {
     this.service.putTodolist().subscribe(
       res => {
         this.resetForm(form);
-        this.toastr.info('Alterado com sucesso', 'Registro de Tarefa');
+        this.toastr.info("Alterado com sucesso", "Registro de Tarefa");
         this.service.refreshList();
       },
       err => {
         console.log(err);
       }
-    )
+    );
   }
-
 }
