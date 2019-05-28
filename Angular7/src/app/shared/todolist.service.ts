c7f3d3abebe8acae2f0a214d4c9a800aec448c04
frianelli-lopes <frianelli.lopes@gmail.com>
2019-05-28 15:08:08 -0300

    Acertando a inclusão

--- Angular7/src/app/shared/todolist.service.ts
+++ Angular7/src/app/shared/todolist.service.ts
@@ -1,31 +1,39 @@
-import { Todolist } from './todolist.model';
-import { Injectable } from '@angular/core';
+import { Todolist } from "./todolist.model";
+import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { API } from "./constants";
+import { map, catchError, take, tap } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class TodolistService {
   formData: Todolist;
-  readonly rootURL = API; 
-  list : Todolist[];
+  readonly rootURL = API;
+  list: Todolist[];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  postTodolist() {
-    return this.http.post(this.rootURL + '/Todolist', this.formData);
+  postTodolist(todo: Todolist) {
+    console.log("postTodolist");
+    console.log(todo);
+    return this.http.post(`${this.rootURL}/ToDoList`, todo);
   }
+
   putTodolist() {
-    return this.http.put(this.rootURL + '/Todolist/'+ this.formData.ToDoListId, this.formData);
+    return this.http.put(
+      this.rootURL + "/Todolist/" + this.formData.ToDoListId,
+      this.formData
+    );
   }
   deleteTodolist(id: any) {
-    return this.http.delete(this.rootURL + '/Todolist/'+ id);
+    return this.http.delete(this.rootURL + "/Todolist/" + id);
   }
 
-  refreshList(){
-    this.http.get(this.rootURL + '/Todolist')
-    .toPromise()
-    .then(res => this.list = res as Todolist[]);
+  refreshList() {
+    this.http
+      .get(this.rootURL + "/Todolist")
+      .toPromise()
+      .then(res => (this.list = res as Todolist[]));
   }
 }
